ea31b281dd5b3975f4349466be51ab951dd653e5
GCM Team <kinhdoanhgreencm@gmail.com>
2026-01-03 01:39:10 +0700

    push

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { createFriendlyUrl } from '@/lib/urlUtils';
-import { Facebook, Instagram, Youtube, Phone, Mail, MapPin } from 'lucide-react';
+import { Facebook, Youtube, Phone, Mail, MapPin } from 'lucide-react';
 
 const Footer: React.FC = () => {
   const scrollToTop = () => {
@@ -71,9 +71,13 @@ const Footer: React.FC = () => {
               </ul>
 
               <div className="flex gap-4 mt-6">
-                <a href="#" className="bg-gcm-green/20 p-2 rounded-full text-gcm-green hover:bg-gcm-green hover:text-black transition-colors"><Instagram size={20} /></a>
-                <a href="#" className="bg-gcm-green/20 p-2 rounded-full text-gcm-green hover:bg-gcm-green hover:text-black transition-colors"><Youtube size={20} /></a>
-                <a href="#" className="bg-gcm-green/20 p-2 rounded-full text-gcm-green hover:bg-gcm-green hover:text-black transition-colors"><Facebook size={20} /></a>
+                <a href="https://www.tiktok.com/@gcm.all.about.car" target="_blank" rel="noopener noreferrer" className="bg-gcm-green/20 p-2 rounded-full text-gcm-green hover:bg-gcm-green hover:text-black transition-colors">
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"/>
+                  </svg>
+                </a>
+                <a href="https://www.youtube.com/@Green-CM" target="_blank" rel="noopener noreferrer" className="bg-gcm-green/20 p-2 rounded-full text-gcm-green hover:bg-gcm-green hover:text-black transition-colors"><Youtube size={20} /></a>
+                <a href="https://www.facebook.com/kinhdoanhgreencm" target="_blank" rel="noopener noreferrer" className="bg-gcm-green/20 p-2 rounded-full text-gcm-green hover:bg-gcm-green hover:text-black transition-colors"><Facebook size={20} /></a>
               </div>
             </div>
           </div>
